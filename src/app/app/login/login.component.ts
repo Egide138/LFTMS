30c4c5e593b145fef7ac7b4b8ed44e20d43bd971
Egide Isabwe <eisabwe@andrew.cmu.edu>
2022-08-01 16:10:43 +0200

    integration

--- src/app/app/login/login.component.ts
+++ src/app/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public LoginForm!: FormGroup;
+  constructor(private build: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.LoginForm = this.build.group({
+      email: [''],
+      Password: [''],
+    })
+  }
+  login() {
+    this.http.get<any>('')
+      .subscribe(res => {
+        const user = res.find((a: any) => {
+          return a.email === this.LoginForm.value.email && a.Password === this.LoginForm.value.Password
+        })
+        if (user) {
+          alert("Login Successful!")
+          this.router.navigate(['UserDashboard'])
+        }
+        else {
+          alert("User not Found! May be you need to register First")
+        }
+
+      }, err => {
+        alert("Something went Wrong!")
+      })
+
   }
 
 }
